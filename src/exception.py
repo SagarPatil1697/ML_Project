967e23612b4cc15c1d8c94326c30fe776b0858c8
SagarPatil1697 <sgrpatil1697@gmail.com>
2023-08-17 12:55:19 +0200

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,16 @@
+import sys
+import logging
+
+def error_message_detail(error,error_detail):
+    _,_,exc_tb = error_detail.exc_info()
+    
+    error_message = "Error occured in python script name [{0}] at line number [{1}] with error message [{2}]".format(exc_tb.tb_frame.f_code.co_filename,exc_tb.tb_lineno,str(error))
+    return error_message
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(self,error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+
